77407f51fd5e98d52958769d45e39a49a0effb69
Daniel Böök <Daniel.book@hotmail.com>
2015-09-06 16:25:19 +0200

    before merge

--- bootstrap/reader.php
+++ bootstrap/reader.php
@@ -1,9 +1,8 @@
-<?php
-
+<?php 
 
 function setPost()
 {
-      
+    echo '<div class="container">'; 
     echo '<div class="row">';
     echo  '<div class="col-md-6">';
 
@@ -19,7 +18,7 @@ function setPost()
     $src = 'data: '.mime_content_type($image).';base64,'.$imageData;
 
     // Echo out a sample image
-    echo '<img class="img-responsive" id="imageCat" src="' . $src . '">';
+    echo '<img class="img-responsive" class="imageCat" src="' . $src . '">';
 
     $piccounter = $piccounter + 1;
 
@@ -27,44 +26,44 @@ function setPost()
 
         echo '<div class="col-md-6" style="border-style:none;" >';
 
-        echo '<h2 class="date" >2015-09-05 21:37</h2> ';
+        echo '<h2 class="date" >2015-09-05 21:37  </h2> ';
 
-        echo '<p class="message">Mjau! Jag vill in det är kallt ute  </p> ';
+        echo '<p class="message" >Mjau! Jag vill in det är kallt ute  </p> ';
 
-        echo ' <img class="img-responsive" src="css/bubble.png" alt="bubbla">';
+        echo '<img class="center-block" class="img-position" src="css/bubble.png" alt="">';
 
         $add = 1;
     $counter = 0;
-    // file example 1: read a text file into an array, with
+    // file example 1: read a text file into an array, with 
     // each line in a new element
 
     // första raden är tom, andra regn och trejde temp
-    $filename="data.txt";
-    $lines = array();
-    $file = fopen($filename, "r");
-    while(!feof($file)) {
+    $filename="data.txt"; 
+    $lines = array(); 
+    $file = fopen($filename, "r"); 
+    while(!feof($file)) { 
 
-    //read file line by line into a new array element
-        $lines[] = fgets($file, 4096);
+    //read file line by line into a new array element 
+        $lines[] = fgets($file, 4096); 
         $counter = $counter + $add;
-    }
+    } 
 
     if ($lines[($counter - $add)] < 0 && $lines[($counter - add)] >= 0 )
     {
-        echo '<h>snö</h>';
+        echo '<h> snö </h>';
     }
     elseif ($lines[($counter - 2*$add)] > 0)
     {
         echo '<img class="img-position-top" src="moln.png" alt="">';
     }
-    elseif ($lines[($counter - $add)] <= 5)
+    elseif ($lines[($counter - $add)] <= 5) 
     {
         echo '<h>Kallt </h>';
     }
-    elseif ($lines[($counter - $add)] >= 15 )
+    elseif ($lines[($counter - $add)] >= 15 ) 
     {
         echo '<h>varmt </h>';
-    }
+    } 
 
     elseif ($lines[($counter - $add)] < 15 && $lines[($counter - $add)] > 5)
     {
