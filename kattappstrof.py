b702c76d47ee0458fb087ffbf342e4c28dffe4a6
Daniel <daniel.book@hotmail.com>
2015-09-06 09:04:06 +0000

    la till bilder

--- kattappstrof.py
+++ kattappstrof.py
@@ -6,8 +6,9 @@ import subprocess
 
 sensor = 12
 
-def get_file_name():
-    return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
+i = 1
+
+image = "image"
 
 #GPIO.setmode(GPIO.BCM)
 GPIO.setmode(GPIO.BOARD)
@@ -25,8 +26,9 @@ while True:
         	new_state = "HIGH" if current_state else "LOW"
         	print("GPIO pin %s is %s" % (sensor,new_state))
 	if current_state:
-		fileName = get_file_name()
-		cam.capture(fileName)
+		#fileName = get_file_name()
+		cam.capture(image+str(i)+".jpg")
+		i+=1
 		subprocess.call("java -jar /home/pi/East-Sweden-Hack-2015/EastSwedenHack/dist/EastSwedenHack.jar", shell=True)
 		time.sleep(5.0)
 		#break
